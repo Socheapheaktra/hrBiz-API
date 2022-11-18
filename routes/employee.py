096a45e66aea5418a91feb8d08032a21e9803e00
macbookair <socheapheaktra@gmail.com>
2022-11-18 14:56:44 +0700

    added create employee

--- routes/employee.py
+++ routes/employee.py
@@ -1,8 +1,44 @@
 from flask import Blueprint, request, jsonify
 from models.user import UserModel
-from utils.conn import mycursor
+from utils.conn import mycursor, mydb
 
 employee = Blueprint('employee', __name__)
+
+@employee.route('/create', methods=["POST"])
+def create_employee():
+    if not request.data or not request.is_json:
+        response = {
+            "status": False,
+            "message": "Invalid Data"
+        }
+        return jsonify(response)
+    req = request.get_json()
+    if "first_name" not in req or "last_name" not in req or "email" not in req \
+            or "password" not in req or "phone" not in req:
+        response = {
+            "status": False,
+            "message": "Invalid Arguments, Incorrect Parameters"
+        }
+        return jsonify(response)
+    try:
+        sql = 'INSERT INTO tblUser(first_name, last_name, email, password, phone) ' \
+              'VALUES (%s, %s, %s, %s, %s)'
+        values = [req['first_name'], req['last_name'], req['email'], req['password'], req['phone'], ]
+        mycursor.execute(sql, values)
+    except Exception as err:
+        mydb.rollback()
+        response = {
+            "status": False,
+            "message": f"{err}"
+        }
+        return jsonify(response)
+    else:
+        mydb.commit()
+        response = {
+            "status": True,
+            "message": "New Employee added!"
+        }
+        return jsonify(response)
 
 @employee.route('/list-employee', methods=["GET"])
 def get_employee_list():
